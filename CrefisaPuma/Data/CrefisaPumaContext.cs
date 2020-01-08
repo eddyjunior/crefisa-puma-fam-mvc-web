f663616d364ae47e817d05ce240904e99a048483
eddyjunior <neggoh@gmail.com>
2020-01-08 13:20:02 -0200

    palmeiras-campeao-dos-campeoes

---
+++ CrefisaPuma/Data/CrefisaPumaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrefisaPuma.Models
+{
+    public class CrefisaPumaContext : DbContext
+    {
+        public CrefisaPumaContext (DbContextOptions<CrefisaPumaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrefisaPuma.Models.Department> Department { get; set; }
+    }
+}
--- CrefisaPuma/Startup.cs
+++ CrefisaPuma/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CrefisaPuma.Models;
 
 namespace CrefisaPuma
 {
@@ -33,6 +35,10 @@ namespace CrefisaPuma
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CrefisaPumaContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("CrefisaPumaContext"), builder =>
+                        builder.MigrationsAssembly("CrefisaPuma")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
