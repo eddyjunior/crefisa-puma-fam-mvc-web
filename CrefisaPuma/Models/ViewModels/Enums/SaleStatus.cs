e89a7b2e6b72906b85d3a74b6c723f5f8cdeba38
eddyjunior <neggoh@gmail.com>
2020-01-08 14:08:21 -0200

    vamos indo

---
+++ CrefisaPuma/Models/ViewModels/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrefisaPuma.Models.ViewModels.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
